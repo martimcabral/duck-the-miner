2a01ba32377fcca0a5384ead9e2860e67a8ad533
Sr. Patinho <mfcmartimyt@gmail.com>
2024-10-17 12:10:46 +0000

    prototype.0.21

--- scripts/MainMenu/options_menu.gd
+++ scripts/MainMenu/options_menu.gd
@@ -7,28 +7,32 @@ func _on_back_button_pressed() -> void:
 
 func _on_display_button_pressed() -> void:
 	$DisplayPanel.visible = true
-	if get_viewport().get_visible_rect().size == Vector2(1920, 1080):
+	$AudioPanel.visible = false
+	$ControlsPanel.visible = false
+	$AccessibilityPanel.visible = false
+	print(get_viewport().get_visible_rect().size)
+	if get_viewport().get_visible_rect().size == Vector2(1921, 1080):
 		$DisplayPanel/WindowsSizeDropDown.selected = 2
 
 func _on_windows_type_drop_down_item_selected(index: int) -> void:
 	match index:
-		0:
+		0: # Fullscreen
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
 			print("Window Mode changed to: ", DisplayServer.window_get_mode())
-		1:
+		1: # Bordeless
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
 			print("Window Mode changed to: ", DisplayServer.window_get_mode())
-
+			
 func _on_windows_size_drop_down_item_selected(index: int) -> void:
 	match index:
 		1:
 			DisplayServer.window_set_size(Vector2i(2560, 1440))
 			print("Resolution Changed to: ", DisplayServer.window_get_size())
-
 		2:
 			DisplayServer.window_set_size(Vector2i(1920, 1080))
 			print("Resolution Changed to: ", DisplayServer.window_get_size())
-
 		3:
 			DisplayServer.window_set_size(Vector2i(1600, 900))
 			print("Resolution Changed to: ", DisplayServer.window_get_size())
@@ -57,3 +61,61 @@ func _on_windows_size_drop_down_item_selected(index: int) -> void:
 			DisplayServer.window_set_size(Vector2i(1024, 768))
 			print("Resolution Changed to: ", DisplayServer.window_get_size())
 			
+
+func _on_vsync_check_button_toggled(toggled_on: bool) -> void:
+	match toggled_on:
+		true:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+			print("Vsync Enabled")
+		false:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+			print("Vsync Disabled")
+
+
+func _on_fps_type_item_selected(index: int) -> void:
+	match index:
+		0:
+			Engine.max_fps = -1
+		1:
+			Engine.max_fps = 360
+		2:
+			Engine.max_fps = 240
+		3:
+			Engine.max_fps = 165
+		4:
+			Engine.max_fps = 144
+		5: 
+			Engine.max_fps = 120
+		6:
+			Engine.max_fps = 100
+		7:
+			Engine.max_fps = 75
+		8:
+			Engine.max_fps = 60
+		9:
+			Engine.max_fps = 45
+		10:
+			Engine.max_fps = 30
+		11:
+			Engine.max_fps = 15
+		12:
+			Engine.max_fps = 1
+	
+
+func _on_audio_button_pressed() -> void:
+	$DisplayPanel.visible = false
+	$AudioPanel.visible = true
+	$ControlsPanel.visible = false
+	$AccessibilityPanel.visible = false
+
+func _on_controls_button_pressed() -> void:
+	$DisplayPanel.visible = false
+	$AudioPanel.visible = false
+	$ControlsPanel.visible = true
+	$AccessibilityPanel.visible = false
+
+func _on_accessibility_button_pressed() -> void:
+	$DisplayPanel.visible = false
+	$AudioPanel.visible = false
+	$ControlsPanel.visible = false
+	$AccessibilityPanel.visible = true
