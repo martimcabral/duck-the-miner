fc8e57f062bfb192737b99d269a51d1fda4d7848
martimcabral <mfcmartimyt@gmail.com>
2024-10-17 19:13:04 +0000

    prototype.0.21

--- scripts/MainMenu/start.gd
+++ scripts/MainMenu/start.gd
@@ -4,46 +4,39 @@ var window_mode = 0
 var agachado = 0
 
 func _ready() -> void:
-	# Assume you have a ColorRect named "ColorRect"
-	var shader_material = $ColorRect.material as ShaderMaterial
-
-	# Set the mode: 
-	# 0 = Normal, 1 = Deuteranopia, 2 = Protanopia, 3 = Tritanopia, 4 = Achromatopsia
-	shader_material.set_shader_parameter("mode", 1)
-	
-	$CanvasLayer/Center/StartMenu.visible = true
-	$CanvasLayer/Center/OptionsMenu.visible = false
+	$GUI/Center/StartMenu.visible = true
+	$GUI/Center/OptionsMenu.visible = false
 	
 	var random_player = randi_range(1, 2)
 	match random_player:
 		1:
-			$CanvasLayer/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_up.png")
+			$GUI/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_up.png")
 		2:
-			$CanvasLayer/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_down.png")
+			$GUI/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_down.png")
 	
 	var random_side = randi_range(1, 2)
 	if random_side == 1:
-		$CanvasLayer/Center/Background.flip_h = true
+		$GUI/Center/Background.flip_h = true
 			
 	var random_flip_y = randi_range(1, 100)
 	if random_flip_y == 1:
-		$CanvasLayer/Center/Background.flip_v = true
+		$GUI/Center/Background.flip_v = true
 	
 	var easter_egg_title = randi_range(1, 20)
 	if easter_egg_title == 20:
-		$CanvasLayer/Center/Title.text = "Miner the Duck"
+		$GUI/Center/Title.text = "Miner the Duck"
 
 func _process(_delta: float) -> void:
-	$CanvasLayer/Center/Background.size = get_viewport_rect().size
+	$GUI/Center/Background.size = get_viewport_rect().size
 	
 	# Agachar no Menu
 	if Input.is_action_just_pressed("Agachar"):
 		if agachado == 0:
 			agachado = 1
-			$CanvasLayer/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_down.png")
+			$GUI/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_down.png")
 		elif agachado == 1:
 			agachado = 0
-			$CanvasLayer/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_up.png")
+			$GUI/Center/Background.texture = ResourceLoader.load("res://assets/textures/main_menu_up.png")
 	
 	# Fullscreen
 	if Input.is_action_just_pressed("Fullscreen"):
