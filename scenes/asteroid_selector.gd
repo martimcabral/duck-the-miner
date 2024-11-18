7868becb275c91c6be896b9913b6c8d9cd86ee4b
Sr. Patinho <mfcmartimyt@gmail.com>
2024-11-18 16:27:44 -0100

    alpha.1.3

--- scenes/asteroid_selector.gd
+++ scenes/asteroid_selector.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+var min_zoom = 0.05
+var max_zoom = 4
+
 func _ready() -> void:
-	
 	if DiscordRPC.get_is_discord_working():
 		var random = randi_range(1, 2)
 		match random:
@@ -9,7 +11,29 @@ func _ready() -> void:
 				DiscordRPC.details = "🌌 Choosing where to go"
 				DiscordRPC.refresh()
 			2:
-				DiscordRPC.details = "🔎 Choosing the next Adventure"
+				DiscordRPC.details = "🔎 Choosing next Adventure"
 				DiscordRPC.refresh()
 	else:
 		print("[discordRP.gd] Discord isn't running or wasn't detected properly, skipping rich presence.") 
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Universe_Zoom_In") and $SolarSystem.scale.x <= max_zoom:
+		$SolarSystem.scale += Vector2(0.05, 0.05)
+		
+	if Input.is_action_just_pressed("Universe_Zoom_Out") and $SolarSystem.scale.x >= min_zoom:
+		$SolarSystem.scale -= Vector2(0.05, 0.05)
+	
+	$UniverseBackground.position = (get_global_mouse_position() * 0.008) + Vector2(-1200, -600)
+	$SolarSystem/Mercury.rotation += delta * 0.05
+	$SolarSystem/Venus.rotation += delta * 0.075
+	$SolarSystem/Earth.rotation += delta * 0.1
+	$SolarSystem/Earth/Moon.rotation += delta * 0.25
+	$SolarSystem/Mars.rotation += delta * 0.125
+	$SolarSystem/DeltaField.rotation += delta * 0.05
+	$SolarSystem/Jupiter.rotation += delta * 0.15
+	$SolarSystem/GammaField.rotation += delta * 0.15
+	$SolarSystem/OmegaField.rotation += delta * 0.15
+	$SolarSystem/Saturn.rotation += delta * 0.25
+	$SolarSystem/Uranus.rotation += delta * 0.35
+	$SolarSystem/Neptune.rotation += delta * 0.33
+	$SolarSystem/LambdaSigma.rotation += delta * 0.005
