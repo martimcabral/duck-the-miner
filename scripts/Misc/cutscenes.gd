b854a4b6cf68ac6896eff9532e357bafeb84bade
Sr. Patinho <mfcmartimyt@gmail.com>
2025-05-22 17:30:11 +0000

    beta.3.7a

--- scripts/Misc/cutscenes.gd
+++ scripts/Misc/cutscenes.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 @onready var TextDisplay = $TextDisplay
-var current_text : String = "S1T1"
+var current_text : String = "T1"
 var fadein = -20
 var year = Time.get_datetime_dict_from_system().year
 var above_sign : bool = false
@@ -18,31 +18,21 @@ func _ready() -> void:
 	$FyctionContract/PrintingAnimation.play("RESET")
 
 var IntroCusceneTexts : Dictionary = {
-	"NOTEXT": "",
-	"S1T1": "In a near future, humanity’s misuse of Earth destroyed the planet and their chances of survival, after this disaster, only some animals could barely live.",
-	"S1T2": "As Earth’s resources ran low, these clever animals started developing new ways to survive, with this, they started helping each other to restore Earth.",
-	"S1T3": "Determined to survive, these animals began to replicate what humans once did, trying their best to mimic their behavior and rebuild what was lost.",
-	"S1T4": "Over time, they developed rational thought and a deeper sense of consciousness, shaping a new Earth.",
-	"S1T5": "1623 Years Later...",
-	"S2T1": "Dan the Duck was a very happy duckling, having the best childhood, even though he didn’t do very well in school.",
-	"S2T2": "Unfortunately, age caught up, and his grades were not very good. So, he decided that school just wasn’t his thing and dropped out.",
-	"S2T3": "Financially, Dan the Duck was not in a good way. After working multiple horrible jobs, he couldn’t even afford his nest rent or old rice.",
-	"S2T4": "Over the years, he kept accumulating debt after debt after debt until he finally gave up and knew exactly what he had to do.",
-	"S2T5": "Apply for the most life risking job. It was that or [shake rate=35 level=10]starve to death[/shake]."
+	"T1": "In the near future, humanity’s abuse of Earth led to its downfall, leaving only a few resilient animals struggling to survive.",
+	"T2": "Together, they developed new ways to live, gradually gaining consciousness and rebuilding a new Earth inspired by the ruins of human civilization.",
+	"T3": "Dan the Duckling had a joyful childhood but struggled in school, eventually decided to drop out as his grades got worse.",
+	"T4": "As an adult, he faced harsh realities: low paying jobs, mounting debts, and barely enough to pay rent or buy old rice.",
+	"T5": "With no way out, Dan made a desperate choice: apply for the most life risking job he could find it was that or [shake rate=35 level=10]starve to death[/shake].",
+	"EMPTY": "",
 }
 
 func _on_timer_timeout() -> void:
 	match current_text:
-		"S1T1": TextDisplay.text = IntroCusceneTexts["S1T2"]; current_text = "S1T2"; $TextTimer.wait_time = 14
-		"S1T2": TextDisplay.text = IntroCusceneTexts["S1T3"]; current_text = "S1T3"; $TextTimer.wait_time = 14
-		"S1T3": TextDisplay.text = IntroCusceneTexts["S1T4"]; current_text = "S1T4"; $TextTimer.wait_time = 5
-		"S1T4": TextDisplay.text = IntroCusceneTexts["S1T5"]; current_text = "S1T5"; $TextTimer.wait_time = 14
-		"S1T5": TextDisplay.text = IntroCusceneTexts["S2T1"]; current_text = "S2T1"; $TextTimer.wait_time = 14
-		"S2T1": TextDisplay.text = IntroCusceneTexts["S2T2"]; current_text = "S2T2"; $TextTimer.wait_time = 14
-		"S2T2": TextDisplay.text = IntroCusceneTexts["S2T3"]; current_text = "S2T3"; $TextTimer.wait_time = 14
-		"S2T3": TextDisplay.text = IntroCusceneTexts["S2T4"]; current_text = "S2T4"; $TextTimer.wait_time = 14
-		"S2T4": TextDisplay.text = IntroCusceneTexts["S2T5"]; current_text = "S2T5"; $TextTimer.wait_time = 14
-		"S2T5": fyction_contract()
+		"T1": TextDisplay.text = IntroCusceneTexts["T2"]; current_text = "T2"
+		"T2": TextDisplay.text = IntroCusceneTexts["T3"]; current_text = "T3"
+		"T3": TextDisplay.text = IntroCusceneTexts["T4"]; current_text = "T4"
+		"T4": TextDisplay.text = IntroCusceneTexts["T5"]; current_text = "T5"
+		"T5": fyction_contract()
 	fadein = -20
 	update_bbcode()
 
