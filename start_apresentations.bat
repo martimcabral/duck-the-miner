2274dbea455759533aa793c6f840284daeadc85c
ATLAS <83667953+martimcabral@users.noreply.github.com>
2025-08-09 15:17:20 +0000

    Delete start_apresentations.bat

--- start_apresentations.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-start "" "https://github.com/martimcabral/duck-the-miner"
-start "" "https://itch.io/game/summary/3038442"
-start "" "https://trello.com/b/oDFcr4Q6/pap-duck-the-miner-release-118"
-
-cd /d "C:\Users\%USERNAME%\Documents\GitHub\duck-the-miner"
-start "" code .
-
-start "" "C:\Users\%USERNAME%\Desktop\godot.exe" -e "C:\Users\%USERNAME%\Documents\GitHub"
